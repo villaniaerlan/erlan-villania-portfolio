1d67a7abbf08b9396f340320c7f874fddb9e9ca9
Erlan Villania <villaniaerlan@gmail.com>
2026-09-14 18:42:54 +0800

    Fix Resend API module format

--- api/send.js
+++ api/send.js
@@ -1,8 +1,8 @@
-const { Resend } = require('resend');
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({
       error: 'Method not allowed',
@@ -67,4 +67,4 @@ module.exports = async (req, res) => {
       error: error.message || 'Something went wrong.',
     });
   }
-};
+}
